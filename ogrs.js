048a7825eb0ba59c5ebda68fa259d92104a98ab7
dnaf <dnaf@users.noreply.github.com>
2016-06-26 10:12:54 -0400

    Update ogrs.js

--- ogrs.js
+++ ogrs.js
@@ -2,7 +2,7 @@ var ce = document.getElementById("ogrsCanvas");
 var c = ce.getContext("2d");
 var f = document.getElementById("ogrsForm");
 var ogrsLogo = new Image();
-ogrsLogo.src = "http://www.rectar2.net/ogrs/graphics/x2/logo_black.png";
+ogrsLogo.src = ".//graphics/x2/logo_black.png";
 
 var ratingImg = {
     "Y": new Image(),
@@ -10,10 +10,10 @@ var ratingImg = {
     "T": new Image(),
     "A": new Image()
 }
-ratingImg.Y.src = "graphics/x2/rating_icon_y.png";
-ratingImg.P.src = "graphics/x2/rating_icon_p.png";
-ratingImg.T.src = "graphics/x2/rating_icon_t.png";
-ratingImg.A.src = "graphics/x2/rating_icon_a.png";
+ratingImg.Y.src = "./graphics/x2/rating_icon_y.png";
+ratingImg.P.src = "./graphics/x2/rating_icon_p.png";
+ratingImg.T.src = "./graphics/x2/rating_icon_t.png";
+ratingImg.A.src = "./graphics/x2/rating_icon_a.png";
 
 var ratingBannerImg = {
     "Y": new Image(),
@@ -21,10 +21,10 @@ var ratingBannerImg = {
     "T": new Image(),
     "A": new Image()
 }
-ratingBannerImg.Y.src = "graphics/x2/rating_banner_y.png";
-ratingBannerImg.P.src = "graphics/x2/rating_banner_p.png";
-ratingBannerImg.T.src = "graphics/x2/rating_banner_t.png";
-ratingBannerImg.A.src = "graphics/x2/rating_banner_a.png";
+ratingBannerImg.Y.src = "./graphics/x2/rating_banner_y.png";
+ratingBannerImg.P.src = "./graphics/x2/rating_banner_p.png";
+ratingBannerImg.T.src = "./graphics/x2/rating_banner_t.png";
+ratingBannerImg.A.src = "/graphics/x2/rating_banner_a.png";
 
 function generate() {
     reasons = [];
